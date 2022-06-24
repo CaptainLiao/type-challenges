f7b1a5baecbdecaf991900710dbaa0c9839eb18d
liaofeiyin <13296605969@163.com>
2022-06-24 15:19:24 +0800

    feat: includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any
+// https://github.com/microsoft/TypeScript/issues/27024#issuecomment-421529650
+type MaybeEquals<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2)
+  ? true
+  : false
+
+
+type Includes<T extends readonly any[], U> = T extends [infer M, ...infer R] 
+  ? MaybeEquals<U, M> extends true ? true : Includes<R, U>
+  : false
+
