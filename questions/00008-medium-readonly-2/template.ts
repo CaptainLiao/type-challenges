432a368cf1469aac73c1e17e9537825b8e32ed88
liaofeiyin <13296605969@163.com>
2022-07-01 18:00:57 +0800

    feat: 00008

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,7 @@
-type MyReadonly2<T, K> = any
+// = keyof T：K 的默认值
+// 使用交叉类型
+// 首先让 T 的所有 key 都 readonly；
+// 然后使用 Omit 去掉非 K 中的 readonly
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [Key in keyof T] : T[Key]
+} & Omit<T, K>
