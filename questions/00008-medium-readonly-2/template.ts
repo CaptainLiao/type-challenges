97b85d38b5e3fb0c0c5f348aaf45c078f6518c08
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-07-02 01:58:30 +0000

    chore: update index

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -3,5 +3,5 @@
 // 首先让 T 的所有 key 都 readonly；
 // 然后使用 Omit 去掉非 K 中的 readonly
 type MyReadonly2<T, K extends keyof T = keyof T> = {
-  readonly [Key in keyof T] : T[Key]
+  readonly [Key in keyof T]: T[Key]
 } & Omit<T, K>
