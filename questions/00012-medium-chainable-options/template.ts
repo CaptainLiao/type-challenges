aa57b72ccd9e375688542b60e4561e5d601db1d5
liaofeiyin <13296605969@163.com>
2022-07-07 09:57:47 +0800

    feat: add 00012

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,7 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+// return Chainable<T> 实现 return this 的效果
+// get(): T, 此时的 T 是多次 Chainable 后的结果
+
+type Chainable<T = {}> = {
+  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<T & Record<K, V>>
+  get(): T
 }
