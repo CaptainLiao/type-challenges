e1d5708cc6ce1b0ec2b38a56011e80ecf3fbd862
liaofeiyin <13296605969@163.com>
2022-07-18 12:12:17 +0800

    feat: add 00296

--- questions/00296-medium-permutation/template.ts
+++ questions/00296-medium-permutation/template.ts
@@ -1,1 +1,11 @@
-type Permutation<T> = any
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+
+// distributive-conditional-types
+// 当 T 是联合类型 A|B|C，T extends U ? X : Y 会被展开成新的联合类型：
+// A extends U ? X : Y | B extends U ? X : Y | C extends U ? X : Y
+
+type Permutation<T, U = T> = [T] extends [never] 
+  ? [] 
+  : T extends U
+    ? [T, ...Permutation<Exclude<U, T>>] 
+    : []
--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,5 @@
-type LengthOfString<S extends string> = any
+// 把字符串转化为数组
+
+type LengthOfString1<S extends string, A extends string[] = []> = S extends `${infer Head}${infer Rest}`
+  ? LengthOfString1<Rest, [...A, Head]>
+  : A['length']
