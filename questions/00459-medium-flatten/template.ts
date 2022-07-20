83082acad10b117314241d0f969447f8b726caa0
liaofeiyin <13296605969@163.com>
2022-07-20 12:09:42 +0800

    feat: add 00459

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,12 @@
-type Flatten = any
+// type Flatten<T extends unknown[], Res extends unknown[] = []> = T extends [infer Head, ...infer Rest]
+//   ? Head extends unknown[] 
+//     ? Flatten<[...Head, ...Rest], Res>
+//     : Flatten<Rest, [...Res, Head]>  
+//   : Res
+
+
+type Flatten<T> = T extends []
+  ? []
+  : T extends [infer F, ...infer Rest]
+    ? [...Flatten<F>, ...Flatten<Rest>]
+    : [T]
