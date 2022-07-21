7f9f6098e33a5df07c2ad9e8f82820d82c6dbfb6
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-07-21 02:02:28 +0000

    chore: update index

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,9 +1,8 @@
 // type Flatten<T extends unknown[], Res extends unknown[] = []> = T extends [infer Head, ...infer Rest]
-//   ? Head extends unknown[] 
+//   ? Head extends unknown[]
 //     ? Flatten<[...Head, ...Rest], Res>
-//     : Flatten<Rest, [...Res, Head]>  
+//     : Flatten<Rest, [...Res, Head]>
 //   : Res
-
 
 type Flatten<T> = T extends []
   ? []
