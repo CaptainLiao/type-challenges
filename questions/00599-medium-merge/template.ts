f87e8e12058db7801d40ef5511e31542de19c87b
liaofeiyin <13296605969@163.com>
2022-07-27 18:21:31 +0800

    feat: add 00599

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,1 +1,5 @@
-type Merge<F, S> = any
+type Merge<F extends object, S extends object> = {
+  [Key in keyof F | keyof S]: Key extends keyof S 
+    ? S[Key] 
+    : Key extends keyof F ? F[Key] : never
+}
