55403f0ca80df3becc264293edccd70938eed39a
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-07-28 02:00:04 +0000

    chore: update index

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,5 +1,5 @@
 type Merge<F extends object, S extends object> = {
-  [Key in keyof F | keyof S]: Key extends keyof S 
-    ? S[Key] 
+  [Key in keyof F | keyof S]: Key extends keyof S
+    ? S[Key]
     : Key extends keyof F ? F[Key] : never
 }
