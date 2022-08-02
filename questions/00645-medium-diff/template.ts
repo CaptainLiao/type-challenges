73fe2696e2b6722657a099b5e6f01de894551b2d
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-08-02 02:20:25 +0000

    chore: update index

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,5 +1,3 @@
 type Diff<O extends object, O1 extends object> = {
   [K in keyof Omit<Merge<O, O1>, keyof O> | keyof Omit<Merge<O, O1>, keyof O1> ]: K extends keyof O ? O[K] : K extends keyof O1 ? O1[K] : never
 }
-
-
