0f5960e5a8d54ce89a9b8b6952404ea419535212
liaofeiyin <13296605969@163.com>
2022-08-04 12:10:17 +0800

    feat: add 00949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,7 @@
-type AnyOf<T extends readonly any[]> = any
+type Truthy<T> = T extends '' | 0 | [] | {} | false 
+  ? false 
+  : true
+
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? F extends Truthy<F> ? true : AnyOf<R>
+  : false
