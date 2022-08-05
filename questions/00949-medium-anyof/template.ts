3ee43ba69c3cb87441ca1b81facb65fb106c226e
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-08-05 01:59:42 +0000

    chore: update index

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,5 +1,5 @@
-type Truthy<T> = T extends '' | 0 | [] | {} | false 
-  ? false 
+type Truthy<T> = T extends '' | 0 | [] | {} | false
+  ? false
   : true
 
 type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
