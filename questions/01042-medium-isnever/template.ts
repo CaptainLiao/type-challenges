3405cd7c366f4b28c4ba087009c18494ea055eef
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-08-08 01:56:04 +0000

    chore: update index

--- questions/01042-medium-isnever/template.ts
+++ questions/01042-medium-isnever/template.ts
@@ -2,4 +2,4 @@
 // since there are no members in this union and thus the result is never.
 // see: https://github.com/microsoft/TypeScript/issues/31751
 
-type IsNever<T> =  [T] extends [never] ? true : false
+type IsNever<T> = [T] extends [never] ? true : false
