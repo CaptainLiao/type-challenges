7d2d72c498d232e679610f71985159524550e08d
liaofeiyin <13296605969@163.com>
2022-08-08 09:00:46 +0800

    feat: add 01042

--- questions/01042-medium-isnever/template.ts
+++ questions/01042-medium-isnever/template.ts
@@ -1,1 +1,5 @@
-type IsNever = any
+// never is a union type, so `T extends never ...` is not applied when T is never.
+// since there are no members in this union and thus the result is never.
+// see: https://github.com/microsoft/TypeScript/issues/31751
+
+type IsNever<T> =  [T] extends [never] ? true : false
