0802126c4dc8e7bf60d2b90248fbbe7f5150b893
liaofeiyin <13296605969@163.com>
2022-08-09 18:37:38 +0800

    feat: add 01097'

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,1 +1,25 @@
-type IsUnion<T> = any
+type NoneUnion<T, F = T> = (
+  T extends F 
+    ? F extends T ? true : false 
+    : never
+  )
+
+type IsUnion<T> = NoneUnion<T> extends true ? false : true
+
+
+// IsUnion<string|number>
+// type t = (string extends string|number
+//   ? (string extends string ? true : false) | (string extends number ? true : false)
+//   : false
+// ) | (number extends string|number
+//   ? (number extends string ? true : false) | (number extends number ? true : false)
+//   :false)
+// >> type t = (true | false) | (false | true)
+// >> type t = boolean | boolean
+// >> type t = boolean
+
+type t2 = NoneUnion<string|number>
+type t3 = NoneUnion<string>
+type t4 = NoneUnion<'a'|'b'|'c'|'d'>
+type t5 = NoneUnion<never>
+
