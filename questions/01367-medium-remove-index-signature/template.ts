cde4275a357b22ad983bf9b6cf4dfa8313c4ec1d
liaofeiyin <13296605969@163.com>
2022-09-19 18:09:17 +0800

    feat: add 01367

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,16 @@
-type RemoveIndexSignature<T> = any
+
+/**
+ * index signature 允许创建的对象不包含任何 key，eg:
+ * type A = {
+ *  [index: string]: string
+ * }
+ * 
+ * const obj:A = {}
+ * 
+ * 所以，可以利用这个特性识别出哪些key是 index signature，即
+ * {} extends {string: ''} => {} extends Recode<K, ''>
+ */
+
+type RemoveIndexSignature<T> = {
+  [K in keyof T as {} extends Record<K, ''> ? never : K]: T[K]
+};
