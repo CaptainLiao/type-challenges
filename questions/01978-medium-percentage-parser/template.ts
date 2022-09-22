7a41b918bca026e80b5eb788d7e7f9e1d9d539e0
liaofeiyin <13296605969@163.com>
2022-09-22 16:46:22 +0800

    feat: add 01978

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,11 @@
-type PercentageParser<A extends string> = any
+
+type PercentageParser2<A extends string> = A extends `${infer X}%`
+  ? [X, '%']
+  : [A, '']
+
+type PercentageParser<A extends string> = A extends `+${infer X}`
+    ? ['+', ...PercentageParser2<X>]
+    : A extends `-${infer X}`
+      ? ['-', ...PercentageParser2<X>]
+      : ['', ...PercentageParser2<A>]
+
