284be61c599c2c8e9b371e258acddc5ff5161b4b
CaptainLiao <CaptainLiao@users.noreply.github.com>
2022-09-23 02:27:11 +0000

    chore: update index

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,11 +1,9 @@
-
 type PercentageParser2<A extends string> = A extends `${infer X}%`
   ? [X, '%']
   : [A, '']
 
 type PercentageParser<A extends string> = A extends `+${infer X}`
-    ? ['+', ...PercentageParser2<X>]
-    : A extends `-${infer X}`
-      ? ['-', ...PercentageParser2<X>]
-      : ['', ...PercentageParser2<A>]
-
+  ? ['+', ...PercentageParser2<X>]
+  : A extends `-${infer X}`
+    ? ['-', ...PercentageParser2<X>]
+    : ['', ...PercentageParser2<A>]
